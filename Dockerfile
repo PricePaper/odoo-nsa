972bbcc9ff220684d2b89211c51a43930455e8a7
Ean J. Price <ean@pricepaper.com>
2020-12-09 22:05:15 -0500

    Split docker build process to speed up development Migrated from Ubuntu to Alpine for container Minor formating improvements

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,21 @@
-FROM ubuntu:focal AS base
+FROM alpine:3.12 AS base
 LABEL maintainer="Ean J Price <ean@pricepaper.com>"
 
 # Generate locale 
 ENV LANG en_US.utf8
 
+COPY geckodriver /usr/local/bin
+
 # Install some deps and wkhtmltopdf
 RUN set -x; \
-        apt-get update \
-        && apt-get install -y locales \
-        && localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8 \
-        && apt-get -y upgrade \
-        && apt-get install -y --no-install-recommends \
-            python3-lxml \
-            firefox-geckodriver \
-            python3-pip \
+        apk update \
+        && apk upgrade \
+        && apk add \
+            py3-lxml \
+            py3-pip \
             firefox \
             dumb-init \
-        && pip3 install --no-cache beautifulsoup4 selenium \
-        && apt-get -y autoclean \
-        && rm -rf /var/lib/apt/lists/*
+        && pip3 install --no-cache beautifulsoup4 selenium
 
 FROM base AS final
 # Copy base script
--- web_scraping.py
+++ web_scraping.py
@@ -8,14 +8,12 @@ import sys
 import time
 import xmlrpc.client
 from argparse import Namespace
-from datetime import datetime
-
 from bs4 import BeautifulSoup
+from datetime import datetime
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import Select
 
 # Log to stdout for containers
 FORMAT = "%(asctime)-15s %(message)s"
@@ -63,10 +61,9 @@ except Exception as e:
 # Browser Configuration
 options = Options()
 options.headless = True
-#options.add_argument("--headless")  # declare the browser to run in headless mode
 
 # Socket Connection Configuration
-socket = xmlrpc.client.ServerProxy(url + '/xmlrpc/object', context=ssl._create_unverified_context())
+socket = xmlrpc.client.ServerProxy(url + '/xmlrpc/object', context=ssl._create_unverified_context(), allow_none=True)
 
 
 def odoo_writeback(create_vals, product_id, write_url=''):
@@ -95,7 +92,7 @@ def random_sleep():
 def restaurant_depot_login(driver, website_config):
     login = False
     while not login:
-         try:
+        try:
             if 'rdepot' in website_config:
                 login_url = website_config['rdepot'][0]
                 username = website_config['rdepot'][1]
@@ -117,9 +114,9 @@ def restaurant_depot_login(driver, website_config):
                 driver.implicitly_wait(60)
 
                 login = True
-         except Exception as e:
-             logging.error("restaurant depot logging in failed. Retrying...")
-             pass
+        except Exception as e:
+            logging.error("restaurant depot logging in failed. Retrying...")
+            pass
     return driver
 
 
@@ -127,37 +124,44 @@ def restaurant_depot_process_page(driver):
     scraped_data = []
     soup_string = BeautifulSoup(driver.page_source, 'lxml')
 
-    for ele in soup_string.findAll('div', {'id': 'items-list'})[0].findAll('ol', {'class': 'products list items product-items'})[0].findAll('li', {'class': 'item product product-item'}):
-        try:
-            event_title = ele.find(class_='col-md-12 data-col').findAll('li')
-            unit_price = ele.find('span', {'class': 'select-price'}) or False
-            case_price = ''
+    try:
+        for ele in soup_string.findAll('div', {'id': 'items-list'})[0].findAll('ol', {
+            'class': 'products list items product-items'})[0].findAll('li', {'class': 'item product product-item'}):
+            try:
+                event_title = ele.find(class_='col-md-12 data-col').findAll('li')
+                unit_price = ele.find('span', {'class': 'select-price'}) or False
+                case_price = ''
 
-            if unit_price:
-                unit_price = unit_price.text.strip().strip('$')
-            else:
-                unit_price = ele.find('div', {'class': 'select-div-box'}) and ele.find('div', {'class': 'select-div-box'}).find('select', {'class': 'product-package-select'}).find('option', {'value': '1'}).text.strip().strip('Unit').strip().strip('$')
-                case_price = ele.find('div', {'class': 'select-div-box'}) and ele.find('div', {'class': 'select-div-box'}).find('select', {'class': 'product-package-select'}).find('option', {'value': '2'}).text.strip().strip('Case').strip().strip('$')
-            if unit_price:
-                product = {}
-                for index, li in enumerate(event_title):
-                    if index == 0:
-                        product['name'] = li.text.strip()
-                    elif index == 1:
-                        product['item'] = li.text.strip('Item:').strip()
-                    elif index == 2:
-                        product['upc'] = li.text.strip('UPC:').strip()
-                    elif index == 3:
-                        product['units_in_case'] = li.text.strip('Units per case:').strip() and float(li.text.strip('Units per case:').strip())
-                        product['case_price'] = case_price and float(case_price)
+                if unit_price:
+                    unit_price = unit_price.text.strip().strip('$')
+                else:
+                    unit_price = ele.find('div', {'class': 'select-div-box'}) and ele.find('div', {
+                        'class': 'select-div-box'}).find('select', {'class': 'product-package-select'}).find('option', {
+                        'value': '1'}).text.strip().strip('Unit').strip().strip('$')
+                    case_price = ele.find('div', {'class': 'select-div-box'}) and ele.find('div', {
+                        'class': 'select-div-box'}).find('select', {'class': 'product-package-select'}).find('option', {
+                        'value': '2'}).text.strip().strip('Case').strip().strip('$')
+                if unit_price:
+                    product = {}
+                    for index, li in enumerate(event_title):
+                        if index == 0:
+                            product['name'] = li.text.strip()
+                        elif index == 1:
+                            product['item'] = li.text.strip('Item:').strip()
+                        elif index == 2:
+                            product['upc'] = li.text.strip('UPC:').strip()
+                        elif index == 3:
+                            product['units_in_case'] = li.text.strip('Units per case:').strip() and float(
+                                li.text.strip('Units per case:').strip())
+                            product['case_price'] = case_price and float(case_price)
 
-                product['unit_price'] = unit_price and float(unit_price)
-                scraped_data.append(product)
-        except Exception as er:
-            logging.error('Exception occured', er)
-
+                    product['unit_price'] = unit_price and float(unit_price)
+                    scraped_data.append(product)
+            except Exception as er:
+                logging.error('Exception occurred', er)
+    except Exception as er:
+        logging.error('Exception occurred', er)
     return scraped_data
-
 
 
 def restaurant_depot_scrape(driver):
@@ -170,21 +174,23 @@ def restaurant_depot_scrape(driver):
         try:
             my_list = driver.find_element_by_xpath("//button[@class='action action-auth-toggle user-shopping-list']")
             my_list.click()
-            link = driver.find_element_by_xpath("//div[@id='header-list-item-count']/div/ol[1]/li[1]/a")   # use li[1] for first list
+            link = driver.find_element_by_xpath(
+                "//div[@id='header-list-item-count']/div/ol[1]/li[1]/a")  # use li[1] for first list
             link.click()
             time.sleep(sleep_time)
-            Select(driver.find_element_by_xpath("/html/body/div[1]/main/div[2]/div[1]/div[2]/div[6]/div/div/div[2]/div/div/select[@id='limiter']")).select_by_value('100')
+            Select(driver.find_element_by_xpath(
+                "/html/body/div[1]/main/div[2]/div[1]/div[2]/div[6]/div/div/div[2]/div/div/select[@id='limiter']")).select_by_value(
+                '100')
             time.sleep(sleep_time)
             page = True
         except Exception as er:
-            if count==3:
+            if count == 3:
                 logging.error("***Restaurant depot Page loading failed.***")
                 return data
             logging.error("restaurant depot Page loading failed. Retrying...")
-            sleep_time+=10
-            count+=1
+            sleep_time += 10
+            count += 1
             driver = driver1
-
 
     while True:
         try:
@@ -192,14 +198,16 @@ def restaurant_depot_scrape(driver):
         except Exception as er:
             logging.error('One page Skipped\n Error:', er)
         try:
-            end_page = driver.find_element_by_xpath("/html/body/div[1]/main/div[2]/div[1]/div[2]/div[6]/div/div/div[3]/div/div[@class='item pages-item-next inactive']")
+            end_page = driver.find_element_by_xpath(
+                "/html/body/div[1]/main/div[2]/div[1]/div[2]/div[6]/div/div/div[3]/div/div[@class='item pages-item-next inactive']")
         except NoSuchElementException:
             end_page = False
         if end_page:
             break
         else:
             try:
-                driver.find_element_by_xpath("/html/body/div[1]/main/div[2]/div[1]/div[2]/div[6]/div/div/div[3]/div/div[3]/a[@class='action  next']").click()
+                driver.find_element_by_xpath(
+                    "/html/body/div[1]/main/div[2]/div[1]/div[2]/div[6]/div/div/div[3]/div/div[3]/a[@class='action  next']").click()
                 time.sleep(20)
             except NoSuchElementException:
                 logging.error('Element not Found')
@@ -212,14 +220,14 @@ def restaurant_depot(products, website_config):
     driver = webdriver.Firefox(options=options)
     driver = restaurant_depot_login(driver, website_config)
     data = restaurant_depot_scrape(driver)
-    data = {elm.get('upc'): {e:elm[e] for e in list(elm.keys()) if e != 'upc'} for elm in data}
+    data = {elm.get('upc'): {e: elm[e] for e in list(elm.keys()) if e != 'upc'} for elm in data}
 
     if 'rdepot' in website_config:
         for sku in list(products.keys()):
-            if sku in list(data.keys()): #product found in the scraped list
-                item_price =  data[sku].get('unit_price')
+            if sku in list(data.keys()):  # product found in the scraped list
+                item_price = data[sku].get('unit_price')
                 if data[sku].get('case_price'):
-                    item_price =  data[sku].get('case_price')
+                    item_price = data[sku].get('case_price')
                 logging.info("writing info sku:%s  Price:%s" % (sku, item_price))
                 create_vals = {'product_sku_ref_id': products[sku][0],
                                'item_name': data[sku].get('name'),
@@ -228,59 +236,64 @@ def restaurant_depot(products, website_config):
                                }
                 res = odoo_writeback(create_vals, products[sku][0])
 
-                schedule_to_unlink = socket.execute(db, login, pwd, 'price.fetch.schedule', 'search', [('product_sku_ref_id', '=', products[sku][0])], 0, 1)
-                unlink_scheduled = schedule_to_unlink and socket.execute(db, login, pwd, 'price.fetch.schedule', 'unlink', schedule_to_unlink)
+                schedule_to_unlink = socket.execute(db, login, pwd, 'price.fetch.schedule', 'search',
+                                                    [('product_sku_ref_id', '=', products[sku][0])], 0, 1)
+                unlink_scheduled = schedule_to_unlink and socket.execute(db, login, pwd, 'price.fetch.schedule',
+                                                                         'unlink', schedule_to_unlink)
 
-            else: #product not found in scraped list log exception
-                write_except = socket.execute(db, login, pwd, 'product.sku.reference', 'log_exception_error', products[sku][0], "Couldn't fetch price due to unknown reason, please check if the product is added in the scrape list setup in restaurant depot website.")
+            else:  # product not found in scraped list log exception
+                write_except = socket.execute(db, login, pwd, 'product.sku.reference', 'log_exception_error',
+                                              products[sku][0],
+                                              "Couldn't fetch price due to unknown reason, please check if the product is added in the scrape list setup in restaurant depot website.")
     return True
 
 
-
-
 def webstaurant_store_fetch(driver, item, products, mode):
-    unit_price = 0
-    product_sku_id = products[item][0]
+    try:
+        unit_price = 0
+        product_sku_id = products[item][0]
 
-    if mode == 'search':
-        search_box = driver.find_element_by_id('searchval')
-        search_box.clear()
-        search_box.send_keys(item)
-        search_button = driver.find_element_by_xpath("//button[@class='bg-origin-box-border bg-repeat-x border-solid border box-border cursor-pointer inline-block font-semibold text-center no-underline hover:no-underline antialiased align-middle hover:bg-position-y-15 rounded-l-none rounded-r-normal box-border text-base-1/2 leading-4 m-0 py-2 px-2 capitalize align-top w-24 z-20 xs:py-3 xs:px-5 xs:w-auto  bg-blue-300 hover:bg-blue-600 text-white text-shadow-black-60 bg-linear-gradient-180-blue border-black-25 shadow-inset-black-17']")
-        search_button.click()
+        if mode == 'search':
+            search_box = driver.find_element_by_id('searchval')
+            search_box.clear()
+            search_box.send_keys(item)
+            search_button = driver.find_element_by_xpath(
+                "//button[@class='bg-origin-box-border bg-repeat-x border-solid border box-border cursor-pointer inline-block font-semibold text-center no-underline hover:no-underline antialiased align-middle hover:bg-position-y-15 rounded-l-none rounded-r-normal box-border text-base-1/2 leading-4 m-0 py-2 px-2 capitalize align-top w-24 z-20 xs:py-3 xs:px-5 xs:w-auto  bg-blue-300 hover:bg-blue-600 text-white text-shadow-black-60 bg-linear-gradient-180-blue border-black-25 shadow-inset-black-17']")
+            search_button.click()
 
+        if mode == 'url':
+            driver.get(products[item][2])
 
-    if mode == 'url':
-        driver.get(products[item][2])
+        driver.implicitly_wait(random.randint(40, 45))
+        item_url = driver.current_url
 
-    driver.implicitly_wait(random.randint(40, 45))
-    item_url = driver.current_url
+        soup_level2 = BeautifulSoup(driver.page_source, 'lxml')
 
-    soup_level2 = BeautifulSoup(driver.page_source, 'lxml')
+        price_tr = soup_level2.findAll('div', {'class': 'pricing'})[0].findAll('tr')
+        price = []
+        name_tag = soup_level2.findAll('h1', {'itemprop': 'Name'})
+        name = False
+        if name_tag:
+            name = name_tag[0].get_text()
+        for tr in price_tr:
+            if tr.find('td'):
+                price.append(
+                    float(tr.find('td').get_text().replace('\n', '').replace('\t', '').replace('$', '').split('/')[0]))
+        if price:
+            unit_price = max(price)
+        else:
+            price_span = soup_level2.findAll('span', {'itemprop': 'price'})
+            if price_span:
+                unit_price = price_span[0].get_text()
 
-    price_tr = soup_level2.findAll('div', {'class': 'pricing'})[0].findAll('tr')
-    price = []
-    name_tag = soup_level2.findAll('h1', {'itemprop': 'Name'})
-    name = False
-    if name_tag:
-        name = name_tag[0].get_text()
-    for tr in price_tr:
-        if tr.find('td'):
-            price.append(
-                float(tr.find('td').get_text().replace('\n', '').replace('\t', '').replace('$', '').split('/')[0]))
-    if price:
-        unit_price = max(price)
-    else:
-        price_span = soup_level2.findAll('span', {'itemprop': 'price'})
-        if price_span:
-            unit_price = price_span[0].get_text()
-
-    if unit_price:
-        logging.info("writing info sku:%s  Price:%s" % (item, unit_price))
-        create_vals = {'product_sku_ref_id': product_sku_id, 'item_name': name, 'item_price': unit_price,
-                       'update_date': str(datetime.now())}
-        res = odoo_writeback(create_vals, product_sku_id, write_url=item_url)
-        return True
+        if unit_price:
+            logging.info("writing info sku:%s  Price:%s" % (item, unit_price))
+            create_vals = {'product_sku_ref_id': product_sku_id, 'item_name': name, 'item_price': unit_price,
+                           'update_date': str(datetime.now())}
+            res = odoo_writeback(create_vals, product_sku_id, write_url=item_url)
+            return True
+    except Exception as er:
+        logging.error('Exception occurred', er)
     return False
 
 
@@ -317,7 +330,7 @@ def webstaurant_store(products, website_config):
                         res = webstaurant_store_fetch(driver, item, products, 'url')
                     random_sleep()
                 except Exception as er:
-                    logging.error('Exception occured for %s: %s' % (item, er))
+                    logging.error('Exception occurred for %s: %s' % (item, er))
                     write_except = socket.execute(db, login, pwd, 'product.sku.reference', 'log_exception_error',
                                                   products[item][0], str(er))
 
